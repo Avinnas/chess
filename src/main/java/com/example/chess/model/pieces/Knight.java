c3c85907a8810df4f0f7b22a8ce60fa16f05444b
xAriess <baranadrian98@gmail.com>
2020-11-05 00:20:49 +0100

    Knight move calculation added

--- src/main/java/com/example/chess/model/pieces/Knight.java
+++ src/main/java/com/example/chess/model/pieces/Knight.java
@@ -4,9 +4,10 @@ import com.example.chess.model.game.Board;
 import com.example.chess.model.game.Color;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @JsonTypeName("Knight")
 public class Knight extends Piece {
@@ -17,14 +18,37 @@ public class Knight extends Piece {
 
     @Override
     public List<Integer> findPossibleMoves(Board board) {
-        int[] relativePossibleMoves = {-17, -15, -10, -6, 6, 10, 15, 17};
-        return Arrays.stream(relativePossibleMoves)
+        List <Integer> relativePossibleMoves = new LinkedList<>(Arrays.asList(-17, -15, -10, -6, 6, 10, 15, 17));
+        List<Integer> moves = new ArrayList<>();
+        int column = tileNumber % 8;
+        int row = tileNumber / 8;
 
-                // TODO - ruchy podczas stania na krawedzi/w rogu
-                .filter(x -> x + tileNumber >= 0)
-                .filter(x -> x + tileNumber < 64)
-                .boxed()
-                .collect(Collectors.toList());
+        if(column < 2){
+            relativePossibleMoves.remove(Integer.valueOf(-10));
+            relativePossibleMoves.remove(Integer.valueOf(6));
+            if(column == 0){
+                relativePossibleMoves.remove(Integer.valueOf(-17));
+                relativePossibleMoves.remove(Integer.valueOf(15));
+            }
+        }
+        if(column>5){
+            relativePossibleMoves.remove(Integer.valueOf(10));
+            relativePossibleMoves.remove(Integer.valueOf(-6));
+            if(row==7){
+                relativePossibleMoves.remove(Integer.valueOf(17));
+                relativePossibleMoves.remove(Integer.valueOf(-15));
+            }
+        }
+        for (int tile : relativePossibleMoves){
+            int realTileNumber = tile + this.tileNumber;
+            if( realTileNumber <64 && realTileNumber >=0 &&
+                    (board.tileIsEmpty(realTileNumber) || board.tileIsOccupiedByOpponent(realTileNumber, this.color)) ){
+                moves.add(realTileNumber);
+            }
+        }
+
+        return moves;
+
     }
 
 }
